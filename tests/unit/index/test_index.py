6ef579119b2007c549f8c6dd1aa4817fa2d75de6
Adrian <adrian@codecov.io>
2022-11-07 16:19:36 -0800

    Adding demo and index as cff; supplying index as regular flag as well

--- tests/unit/index/test_index.py
+++ tests/unit/index/test_index.py
@@ -4,11 +4,11 @@ from src.index.index import Index
 def test_uncovered_if():
     assert Index().uncovered_if() == False
 
-def test_uncovered_if():
-    assert Index().uncovered_if(False) == True
+# def test_uncovered_if():
+#     assert Index().uncovered_if(False) == True
 
-def test_fully_covered():
-    assert Index().fully_covered() == True
+# def test_fully_covered():
+#     assert Index().fully_covered() == True
 
 def test_uncover_covered():
     assert Index().uncovered() == True
